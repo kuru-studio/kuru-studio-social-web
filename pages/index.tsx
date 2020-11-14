5fe7d81b5d704a99f82bcbb226140fbfe93a6f00
BosEriko <bos.eriko@gmail.com>
2020-11-14 13:02:19 +0800

    Feature: Initialize Firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: `G-${process.env.MEASUREMENT_ID}`,
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function HomePage() {
   return <div>Welcome to {process.env.SITE_NAME}!</div>
 }
