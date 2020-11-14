fce54fbe195a43d143103d3d725fc134e91366e6
BosEriko <bos.eriko@gmail.com>
2020-11-14 14:41:37 +0800

    Feature: Simple form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const firebaseConfig = {
+interface IFirebaseConfig {
+  apiKey?: string;
+  authDomain?: string;
+  databaseURL?: string;
+  projectId?: string;
+  storageBucket?: string;
+  messagingSenderId?: string;
+  appId?: string;
+  measurementId?: string;
+}
+
+const firebaseConfig: IFirebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`,
@@ -12,10 +23,27 @@ const firebaseConfig = {
   measurementId: `G-${process.env.MEASUREMENT_ID}`,
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function HomePage() {
-  return <div>Welcome to {process.env.SITE_NAME}!</div>
+  const submitForm = (event) => {
+    alert('test');
+    event.preventDefault();
+    console.log(event.target.email.value);
+    console.log(event.target.password.value);
+  };
+  return (
+    <>
+      <div>Welcome to {process.env.SITE_NAME}!</div>
+      <form onSubmit={submitForm}>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button>submit</button>
+      </form>
+    </>
+  );
 }
 
 export default HomePage
