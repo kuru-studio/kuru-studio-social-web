783e0ac4aa313e921914c902d5541f95000089bb
BosEriko <bos.eriko@gmail.com>
2020-11-14 10:43:07 +0800

    Feature: Add firebase env variables

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ require('dotenv').config();
 module.exports = {
   env: {
     SITE_NAME: process.env.SITE_NAME,
+    API_KEY: process.env.API_KEY,
+    PROJECT_ID: process.env.PROJECT_ID,
+    SENDER_ID: process.env.SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID
   },
 }
