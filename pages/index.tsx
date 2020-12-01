1e93274f62b356d4bd37f260d5c3b2ed88da8b63
BosEriko <bos.eriko@gmail.com>
2020-12-01 22:29:10 +0800

    Feature: Add conditional components on the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,52 +29,52 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+let userLoggedIn: boolean = false;
+
 function listenToCurrentUserState(): void {
   firebase.auth().onAuthStateChanged((user: any): void => {
     if (user) {
       user.getIdToken().then((idToken: string): void => {
         console.log('Token:', `Bearer ${idToken}`);
+        userLoggedIn = true;
       });
       window.sessionStorage.user = JSON.stringify(user);
     } else {
       console.log('Not logged in.');
+      userLoggedIn = false;
     }
   });
 }
 
-function HomePage() {
-  const registerSubmit = (event) => {
-    event.preventDefault();
+const registerSubmit = (event) => {
+  event.preventDefault();
 
-    const emailData = event.target.email.value;
-    const passwordData = event.target.password.value;
+  const emailData = event.target.email.value;
+  const passwordData = event.target.password.value;
 
-    firebase.auth().createUserWithEmailAndPassword(emailData, passwordData).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-  };
+  firebase.auth().createUserWithEmailAndPassword(emailData, passwordData).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+};
 
-  const loginSubmit = (event) => {
-    event.preventDefault();
+const loginSubmit = (event) => {
+  event.preventDefault();
 
-    const emailData = event.target.email.value;
-    const passwordData = event.target.password.value;
+  const emailData = event.target.email.value;
+  const passwordData = event.target.password.value;
 
-    firebase.auth().signInWithEmailAndPassword(emailData, passwordData).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-  };
+  firebase.auth().signInWithEmailAndPassword(emailData, passwordData).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+};
 
-  listenToCurrentUserState();
+listenToCurrentUserState();
 
+function LoggedOutPage() {
   return (
     <>
-      <Head>
-        <title>{process.env.SITE_NAME}</title>
-      </Head>
-      <div>Welcome to {process.env.SITE_NAME}!</div>
       <div>
         <form onSubmit={registerSubmit}>
           <div>Register</div>
@@ -91,6 +91,30 @@ function HomePage() {
           <button>submit</button>
         </form>
       </div>
+    </>
+  );
+}
+
+function LoggedInPage() {
+  return (
+    <>
+      Logged in page.
+    </>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>{process.env.SITE_NAME}</title>
+      </Head>
+      <div>Welcome to {process.env.SITE_NAME}!</div>
+      {
+        userLoggedIn
+          ? <LoggedInPage />
+          : <LoggedOutPage />
+      }
     </>
   );
 }
