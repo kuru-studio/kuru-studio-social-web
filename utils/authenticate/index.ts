7d2c9f0fd5bbe0c34aff23cc79e39f195d4c884d
BosEriko <bos.eriko@gmail.com>
2020-12-08 09:33:42 +0800

    working app

--- utils/authenticate/index.ts
+++ utils/authenticate/index.ts
@@ -25,13 +25,13 @@ interface IFirebaseConfig {
 // ANCHOR Firebase Config
 const _firebaseConfig: IFirebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: `G-${process.env.MEASUREMENT_ID}`,
 };
 
 // ANCHOR Base Authentication
