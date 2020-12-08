54a19b3904ef0ecd4e00ae47377e59ccce4f373f
BosEriko <bos.eriko@gmail.com>
2020-12-08 10:18:10 +0800

    clean the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,13 @@ import "firebase/auth";
 
 import Head from 'next/head'
 
-import { GET } from '../utils/request';
-
-import { LoginForm } from '../components/LoginForm';
-import { RegisterForm } from '../components/RegisterForm';
-
-import { LogOutButton } from '../components/LogOutButton';
-
-function LoggedOutPage() {
-  return (
-    <>
-      <LoginForm />
-      <RegisterForm />
-    </>
-  );
-}
-
 function HomePage() {
   return (
     <>
       <Head>
         <title>{process.env.SITE_NAME}</title>
       </Head>
-      <div>Welcome to {process.env.SITE_NAME}!!!</div>
-      <LoggedOutPage />
+      <div>Welcome to {process.env.SITE_NAME}!</div>
     </>
   );
 }
