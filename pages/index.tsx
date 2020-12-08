a7d4034f7c5389f46588f987a2036ffdc7e8d91e
BosEriko <bos.eriko@gmail.com>
2020-12-08 17:50:58 +0800

    Feature: Add a sign in link on the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import "firebase/auth";
 
 import Head from 'next/head'
 
+import Link from 'next/link'
+
 function HomePage() {
   return (
     <>
@@ -10,6 +12,7 @@ function HomePage() {
         <title>{process.env.SITE_NAME}</title>
       </Head>
       <div>Welcome to {process.env.SITE_NAME}!</div>
+      <div><Link href="/signin">Sign In</Link></div>
     </>
   );
 }
