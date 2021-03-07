5eba70e530fae203f0d6c042fe96fe41b0f02768
BosEriko <bos.eriko@gmail.com>
2021-03-07 12:30:44 +0800

    update constants

--- state/action/constants.ts
+++ state/action/constants.ts
@@ -1,3 +1,3 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const AUTHENTICATE = 'AUTHENTICATE';
+export const INCREMENT    = "INCREMENT";
+export const DECREMENT    = "DECREMENT";
+export const AUTHENTICATE = "AUTHENTICATE";
