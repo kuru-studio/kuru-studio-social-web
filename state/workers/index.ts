9b8593571e9eaca46bb4527f7bc92de55a205373
BosEriko <bos.eriko@gmail.com>
2021-03-09 15:34:34 +0800

    fix the payload overload call

--- state/workers/index.ts
+++ state/workers/index.ts
@@ -2,9 +2,9 @@ import { call, put, takeLatest } from 'redux-saga/effects'
 import { USER_FETCH_FAILED, USER_FETCH_REQUESTED, USER_FETCH_SUCCEEDED } from '../constants';
 import { getUsers } from '../../utilities/request/getUsers';
 
-function* fetchUser(payload) {
+function* fetchUser(payload: any) {
    try {
-      const user = yield call(getUsers(payload), action.payload.userId);
+      const user = yield call(getUsers, payload);
       yield put({type: USER_FETCH_SUCCEEDED, user: user});
    } catch (e) {
       yield put({type: USER_FETCH_FAILED, message: e.message});
