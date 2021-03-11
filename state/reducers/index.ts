5a193fa8f826af15d8b9299459f2537264491fea
BosEriko <bos.eriko@gmail.com>
2021-03-11 11:52:04 +0800

    sort the root reducer alphabetically

--- state/reducers/index.ts
+++ state/reducers/index.ts
@@ -3,14 +3,14 @@ import { combineReducers } from 'redux';
 
 // ANCHOR: Module Imports
 import { counterReducer } from './modules/counterReducer';
-import { userTokenReducer } from './modules/userTokenReducer';
 import { errorMessageReducer } from './modules/errorMessageReducer';
 import { usersListReducer } from './modules/usersListReducer';
+import { userTokenReducer } from './modules/userTokenReducer';
 
 // ANCHOR: Combine Reducers
 export const rootReducers = combineReducers({
   counter: counterReducer,
-  userToken: userTokenReducer,
   errorMessage: errorMessageReducer,
   usersList: usersListReducer,
+  userToken: userTokenReducer,
 });
