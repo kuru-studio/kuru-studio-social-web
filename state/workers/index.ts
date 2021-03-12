e7bac8b71e5d57c57c86018473e2268b98096110
Bos Eriko Reyes <bos.eriko@gmail.com>
2021-03-12 10:12:45 +0800

    Feature: Multiple saga setup (#5)

--- state/workers/index.ts
+++ state/workers/index.ts
@@ -1,13 +1,10 @@
 // ANCHOR: Redux Saga Import
-import { takeLatest } from 'redux-saga/effects'
-
-// ANCHOR: Constant Import
-import { REQUEST_USERS_LIST_CONSTANT } from '@state/constants';
+import { spawn } from 'redux-saga/effects'
 
 // ANCHOR: Worker Import
 import { usersListWorker } from '@state/workers/modules/usersListWorker';
 
-// ANCHOR: Root Saga
-export function* rootSaga() {
-  yield takeLatest(REQUEST_USERS_LIST_CONSTANT, usersListWorker);
+// ANCHOR: Root Workers
+export function* rootWorkers() {
+  yield spawn(usersListWorker)
 }
