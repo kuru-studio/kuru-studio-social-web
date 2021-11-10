603b3298c151b11eb8f1b31465d494b8fc741a26
BosEriko <bos.eriko@gmail.com>
2021-11-10 14:00:11 +0800

    Feature: Add an Example Component for kuru-studio-design usage

--- components/templates/Landing.tsx
+++ components/templates/Landing.tsx
@@ -5,12 +5,16 @@ import Link from 'next/link'
 import FooterOrganism from "@components/organisms/FooterOrganism";
 import HeaderOrganism from "@components/organisms/HeaderOrganism";
 
+// ANCHOR: Kuru Studio Design Import
+import { ExampleComponent } from 'kuru-studio-design'
+
 // ANCHOR: Landing Template
 export default function LandingTemplate() {
   return (
     <>
       <HeaderOrganism />
       <main>
+        <ExampleComponent text="Create React Library Example 😄" />
         <div>Welcome to {process.env.SITE_NAME}!</div>
         <div><Link href="/signin">Sign In</Link></div>
       </main>
