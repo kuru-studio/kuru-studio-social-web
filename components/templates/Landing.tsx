cdecb8feabd66287f5b18bdc8b471efe219f46fb
BosEriko <bos.eriko@gmail.com>
2021-12-08 19:00:11 +0800

    add react grid system

--- components/templates/Landing.tsx
+++ components/templates/Landing.tsx
@@ -5,15 +5,26 @@ import Link from 'next/link'
 import FooterOrganism from "@components/organisms/FooterOrganism";
 import HeaderOrganism from "@components/organisms/HeaderOrganism";
 
+// ANCHOR: React Grid System Import
+import { Container, Row, Col } from 'react-grid-system';
+
 // ANCHOR: Landing Template
 export default function LandingTemplate() {
   return (
     <>
       <HeaderOrganism />
-      <main>
-        <div>Welcome to {process.env.SITE_NAME}!</div>
-        <div><Link href="/signin">Sign In</Link></div>
-      </main>
+      <Container>
+        <Row>
+          <Col>Sidebar 1</Col>
+          <Col>
+            <main>
+              <div>Welcome to {process.env.SITE_NAME}!</div>
+              <div><Link href="/signin">Sign In</Link></div>
+            </main>
+          </Col>
+          <Col>Sidebar 2</Col>
+        </Row>
+      </Container>
       <FooterOrganism />
     </>
   );
