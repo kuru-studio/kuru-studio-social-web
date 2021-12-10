4dc7809a502097c975f236e31f14dda082135b17
BosEriko <bos.eriko@gmail.com>
2021-12-10 18:44:28 +0800

    add interface to user login

--- requests/modules/userLoginRequest.ts
+++ requests/modules/userLoginRequest.ts
@@ -4,7 +4,19 @@ import { POST } from "@requests/index";
 // ANCHOR: GraphQL Definition
 import { userLoginDefinition } from "@definitions/modules/userLoginDefinition";
 
-export async function usersListRequest(email: string, password: string): Promise<any> {
+// ANCHOR User Login Interface
+interface UserLoginInterface {
+  data: {
+    signinUser: {
+      token: string;
+      user: {
+        id: string;
+      };
+    };
+  };
+}
+
+export async function userLoginRequest(email: string, password: string): Promise<UserLoginInterface> {
   const data = await POST('/data', {
     query: userLoginDefinition(email, password),
     headers: {
---
+++ requests/modules/usersListRequest.ts
@@ -0,0 +1,12 @@
+import { GET } from "@requests/index";
+
+export async function usersListRequest(token: any): Promise<any> {
+  const data = await GET('/users', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token.payload}`,
+    },
+  });
+
+  return (await data.json()) as any;
+}
