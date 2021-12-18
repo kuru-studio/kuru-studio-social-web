d4bafbc70460015ef2a832a0cfb299299d80a9c0
BosEriko <bos.eriko@gmail.com>
2021-12-18 20:35:06 +0800

    remove userloginaction for now

--- state/actions/modules/userLoginAction.ts
+++
@@ -1,8 +0,0 @@
-// ANCHOR: Constant Import
-import { USER_LOGIN_CONSTANT } from '@state/constants';
-
-// ANCHOR: User Login Action
-export const userLoginAction = (credentials: string | null) => ({
-  type: USER_LOGIN_CONSTANT,
-  payload: token,
-})
