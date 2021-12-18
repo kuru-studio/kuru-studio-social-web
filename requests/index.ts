f675dd781aa0dd48c5a9d08a49ab833c2f32c4ac
BosEriko <bos.eriko@gmail.com>
2021-12-18 19:52:16 +0800

    rename $apiFetch to $rest and rename gqlRequest to GQL

--- requests/index.ts
+++ requests/index.ts
@@ -4,22 +4,22 @@ import { request, gql } from 'graphql-request'
 // ANCHOR: Declare the API variable
 const API = process.env.API_PATH ?? '';
 
-// ANCHOR: GraphQL Request
-export function gqlRequest(url: string, query?: any) {
-  return request(`${API}${url}`, query);
-}
-
 // ANCHOR: Base Request
-export function $apiFetch(url: string, config?: any) {
+function $rest(url: string, config?: any) {
   return fetch(`${API}${url}`, {
     ...config,
     mode: 'cors',
   });
 }
 
+// ANCHOR: GraphQL Request
+export function GQL(url: string, query?: any) {
+  return request(`${API}${url}`, query);
+}
+
 // ANCHOR: GET Request
 export function GET(url: string, config?: any) {
-  return $apiFetch(url, {
+  return $rest(url, {
     ...config,
     method: 'GET',
   });
@@ -27,7 +27,7 @@ export function GET(url: string, config?: any) {
 
 // ANCHOR: POST Request
 export function POST(url: string, data?: any, config?: any) {
-  return $apiFetch(url, {
+  return $rest(url, {
     ...config,
     method: 'POST',
     body: JSON.stringify(data),
@@ -36,7 +36,7 @@ export function POST(url: string, data?: any, config?: any) {
 
 // ANCHOR: PUT Request
 export function PUT(url: string, data?: any, config?: any) {
-  return $apiFetch(url, {
+  return $rest(url, {
     ...config,
     method: 'PUT',
     body: JSON.stringify(data),
@@ -45,7 +45,7 @@ export function PUT(url: string, data?: any, config?: any) {
 
 // ANCHOR: PATCH Request
 export function PATCH(url: string, data?: any, config?: any) {
-  return $apiFetch(url, {
+  return $rest(url, {
     ...config,
     method: 'PATCH',
     body: JSON.stringify(data),
@@ -54,7 +54,7 @@ export function PATCH(url: string, data?: any, config?: any) {
 
 // ANCHOR: DELETE Request
 export function DELETE(url: string, config?: any) {
-  return $apiFetch(url, {
+  return $rest(url, {
     ...config,
     method: 'DELETE',
   });
