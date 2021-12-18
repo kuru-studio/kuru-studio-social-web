e647785e4caffb1e5174b1e050932360affdcd39
BosEriko <bos.eriko@gmail.com>
2021-12-19 04:57:16 +0800

    add redux support

--- pages/example/login.tsx
+++ pages/example/login.tsx
@@ -3,23 +3,36 @@ import * as React from 'react';
 import { useState } from 'react';
 
 // ANCHOR: Redux
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // ANCHOR: Request
 import { userLoginRequest } from '@requests/modules/userLoginRequest';
+
+// ANCHOR: Redux Actions
+import { userTokenAction } from '@state/actions';
+
+// ANCHOR: Interface
+interface IRootState {
+  counter: string | null;
+}
 
 // ANCHOR: Login Page
 export default () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const dispatch = useDispatch();
+
+  const token = useSelector((state: IRootState) => state.userToken);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(await userLoginRequest({ email, password }));
+    const user = await userLoginRequest({ email, password });
+    dispatch(userTokenAction(user.data.signinUser.token));
   }
 
   return (
     <div>
+      Token:{ ` ${token}` }
       <form onSubmit={handleSubmit}>
         <input
           type="email"
