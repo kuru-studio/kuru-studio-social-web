d22a56e3fad96220eb22c87f4cdde3016a4300cf
BosEriko <bos.eriko@gmail.com>
2021-12-19 19:40:10 +0800

    remove firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import * as React from 'react';
 import App from 'next/app';
 
 // ANCHOR: General Imports
-import { listenToCurrentUserState } from '@utilities/authenticate';
 import { checkUserState } from '@utilities/checkUserState';
 
 // ANCHOR: Redux Imports
 import { reduxStore } from '@state/store';
 import { Provider as ReduxProvider } from 'react-redux';
-
-// ANCHOR: User Current State
-listenToCurrentUserState();
 
 // ANCHOR: Check User State
 checkUserState();
