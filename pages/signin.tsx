650ad1c6b5e26bbd26b13920d1247ebc366723e1
BosEriko <bos.eriko@gmail.com>
2021-12-21 01:04:12 +0800

    use formik for the login

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,5 @@
 // ANCHOR: React
 import * as React from 'react';
-import { useState } from 'react';
 
 // ANCHOR: Redux
 import { useSelector, useDispatch } from 'react-redux';
@@ -12,6 +11,9 @@ import { userLoginRequest } from '@requests/modules/userLoginRequest';
 import { setUserToken } from '@utilities/setUserToken';
 import { clearUserToken } from '@utilities/clearUserToken';
 
+// ANCHOR: Formik
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 // ANCHOR: Interface
 interface IRootState {
   userToken: string | null;
@@ -19,18 +21,9 @@ interface IRootState {
 
 // ANCHOR: Login Page
 export default () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
   const dispatch = useDispatch();
 
   const token = useSelector((state: IRootState) => state.userToken);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const user = await userLoginRequest({ email, password });
-    const token = user.signinUser.token;
-    setUserToken(token);
-  }
 
   const handleLogOut = (event) => {
     event.preventDefault();
@@ -47,23 +40,37 @@ export default () => {
 
   const SignedOutComponent = () => {
     return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e: any) => setEmail(e.target.value)}
-          placeholder="Enter Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e: any) => setPassword(e.target.value)}
-          placeholder="Enter Password"
-          required
-        />
-        <input type="submit" />
-      </form>
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={async (values) => {
+          const user = await userLoginRequest(values);
+          const token = user.signinUser.token;
+          setUserToken(token);
+       }}
+     >
+       {({ isSubmitting }) => (
+         <Form>
+           <Field type="email" name="email" />
+           <ErrorMessage name="email" component="div" />
+           <Field type="password" name="password" />
+           <ErrorMessage name="password" component="div" />
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </Form>
+       )}
+     </Formik>
     );
   }
 
