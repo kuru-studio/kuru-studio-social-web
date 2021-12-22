594fdf01484e6883f2a2aa25a2083e78483dba3d
BosEriko <bos.eriko@gmail.com>
2021-12-22 10:04:43 +0800

    move definitions to their own folder

--- definitions/index.ts
+++ definitions/index.ts
@@ -1,3 +1,3 @@
 // ANCHOR: Export User Definition
-export { userLoginDefinition } from './modules/userLoginDefinition';
-export { userRegisterDefinition } from './modules/userRegisterDefinition';
+export { userLoginDefinition } from './modules/user/userLoginDefinition';
+export { userRegisterDefinition } from './modules/user/userRegisterDefinition';
