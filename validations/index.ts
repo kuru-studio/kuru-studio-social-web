e6fdfd48cf87f2d7d106db5d626f8321e68ad07c
BosEriko <bos.eriko@gmail.com>
2021-12-22 10:21:11 +0800

    move validations to their own folder

--- validations/index.ts
+++ validations/index.ts
@@ -1,5 +1,5 @@
 // ANCHOR: Export User Login Validation
-export { userLoginValidation } from "./modules/userLoginValidation";
+export { userLoginValidation } from "./modules/user/userLoginValidation";
 
 // ANCHOR: Export User Register Validation
-export { userRegisterValidation } from "./modules/userRegisterValidation";
+export { userRegisterValidation } from "./modules/user/userRegisterValidation";
