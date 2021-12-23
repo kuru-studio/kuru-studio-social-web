11cb20ea9f743cbcd30230a95223d60c478c9117
BosEriko <bos.eriko@gmail.com>
2021-12-24 06:05:28 +0800

    remove redux saga

--- state/reducers/index.ts
+++ state/reducers/index.ts
@@ -4,13 +4,11 @@ import { combineReducers } from 'redux';
 // ANCHOR: Module Import
 import { counterReducer } from './modules/counterReducer';
 import { errorMessageReducer } from './modules/errorMessageReducer';
-import { usersListReducer } from './modules/usersListReducer';
 import { userTokenReducer } from './modules/userTokenReducer';
 
 // ANCHOR: Combine Reducers
 export const rootReducers = combineReducers({
   counter: counterReducer,
   errorMessage: errorMessageReducer,
-  usersList: usersListReducer,
   userToken: userTokenReducer,
 });
