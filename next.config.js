e5084424bf66c0d134b60fa24d95b08f0a404dc1
BosEriko <bos.eriko@gmail.com>
2022-01-05 05:36:22 +0800

    update environment variables for ssr

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
   env: {
     SITE_NAME: process.env.SITE_NAME,
-    API_KEY: process.env.API_KEY,
-    PROJECT_ID: process.env.PROJECT_ID,
-    SENDER_ID: process.env.SENDER_ID,
-    APP_ID: process.env.APP_ID,
-    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
     API_PATH: process.env.API_PATH,
+    COOKIE_DURATION: process.env.COOKIE_DURATION,
+    S3_AWS_ACCESS_KEY: process.env.S3_AWS_ACCESS_KEY,
+    S3_AWS_SECRET_KEY: process.env.S3_AWS_SECRET_KEY,
+    S3_AWS_REGION: process.env.S3_AWS_REGION,
+    S3_AWS_BUCKET_NAME: process.env.S3_AWS_BUCKET_NAME
   },
 }
