6b30e8ee7c7f3a86187d46680ae65527e1a09ea1
BosEriko <bos.eriko@gmail.com>
2022-01-05 04:55:05 +0800

    add aws functions

--- utilities/modules/uploadData/index.ts
+++ utilities/modules/uploadData/index.ts
@@ -1,3 +1,26 @@
-export function uploadData(address) {
-  console.log("hoy");
+// ANCHOR: AWS SDK
+import aws from 'aws-sdk';
+
+// ANCHOR: Upload Data
+export function uploadData(req, res) {
+  aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: process.env.AWS_REGION,
+    signatureVersion: 'v4',
+  });
+
+  const s3 = new aws.S3();
+  const post = s3.createPresignedPost({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Fields: {
+      key: req.query.file,
+    },
+    Expires: 60, // seconds
+    Conditions: [
+      ['content-length-range', 0, 1048576], // up to 1 MB
+    ],
+  });
+
+  res.status(200).json(post);
 };
