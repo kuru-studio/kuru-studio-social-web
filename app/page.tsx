860a012a9d7487ff8592a884c23b0583819f20ce
BosEriko <bos.eriko@gmail.com>
2023-10-12 21:43:11 +0800

    add apollo support via ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-export default function HomePage() {
+import { getClient } from "../lib/client";
+import { gql } from "@apollo/client";
+
+export const revalidate = 5;
+const query = gql`query Now {
+    now(id: "1")
+}`;
+
+export default async function Page() {
+  const { data } = await getClient().query({ query });
+
   return (
-    <>
-      <main>
-        Ongoing Maintenance
-      </main>
-    </>
-  )
-};
+    <div>
+      Ongoing Maintenance: {data.now}
+    </div>
+  );
+}
