b2017846d609345523d188896371573f7a941ea2
Bos Eriko Reyes <bos.eriko@gmail.com>
2023-10-19 21:50:34 +0800

    Feature: GraphQL Login and Register (#37)

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { getClient } from "./_utilities/apollo_client";
-import { gql } from "@apollo/client";
-
-export const revalidate = 5;
-const query = gql`query Now {
-    now(id: "1")
-}`;
-
-export default async function Page() {
-  const { data } = await getClient().query({ query });
-
+export default async function HomePage() {
   return (
     <div>
-      Ongoing Maintenance: {data.now}
+      Ongoing Maintenance
     </div>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,29 +2,33 @@
 import { useState } from 'react';
 import { request } from 'graphql-request';
 
-export default function LoginPage() {
+export default function RegisterPage() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSignIn = async () => {
     try {
       const mutation = `
-        mutation signIn($email: String!, $password: String!) {
-          signIn(input: {
-            credentials: {
-              email: $email,
-              password: $password
+        mutation createUser($email: String!, $password: String!, $name: String!) {
+          createUser(input: {
+            name: $name,
+            authProvider: {
+              credentials: {
+                email: $email,
+                password: $password
+              }
             }
           }) {
-            token
-            user {
-              id
-            }
+            id
+            name
+            email
           }
         }
       `;
 
       const variables = {
+        name,
         email,
         password,
       };
@@ -40,6 +44,12 @@ export default function LoginPage() {
     <div>
       <input
         type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
