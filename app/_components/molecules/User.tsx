09652c93660e3f299771d2c3fa4139e600272506
BosEriko <bos.eriko@gmail.com>
2023-10-31 23:32:14 +0800

    make header responsive

--- app/_components/molecules/User.tsx
+++ app/_components/molecules/User.tsx
@@ -2,9 +2,9 @@ import Atom from "@atom";
 
 const User = () => {
   return (
-    <div className="flex gap-3">
-      <Atom.Button>Log In</Atom.Button>
-      <Atom.Button>Register</Atom.Button>
+    <div className="flex gap-3 m-3 md:m-0">
+      <div className="grow md:grow-0"><Atom.Button block className="bg-white text-black">Log In</Atom.Button></div>
+      <div className="grow md:grow-0"><Atom.Button block className="bg-white text-black">Register</Atom.Button></div>
     </div>
   );
 }
