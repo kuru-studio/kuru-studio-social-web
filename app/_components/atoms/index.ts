4e46eeb095667755b62707ed0fe867f3ae2c7631
BosEriko <bos.eriko@gmail.com>
2023-10-31 10:42:39 +0800

    add github button

--- app/_components/atoms/index.ts
+++ app/_components/atoms/index.ts
@@ -2,12 +2,14 @@ import Visibility from "./Visibility";
 import Card from "./Card";
 import Logo from "./Logo";
 import Button from "./Button";
+import GitHub from "./GitHub";
 
 const Atom = {
   Visibility,
   Card,
   Logo,
   Button,
+  GitHub,
 }
 
 export default Atom;
