93ff2909198a4d0ee5dd457c143766d6232c221b
BosEriko <bos.eriko@gmail.com>
2023-11-06 20:49:59 +0800

    add redux

--- app/_components/organisms/Wrapper.tsx
+++ app/_components/organisms/Wrapper.tsx
@@ -1,11 +1,16 @@
+"use client";
 import StyledComponentsRegistry from '../../_utilities/antd_registry';
 import ConfigProvider from 'antd/es/config-provider';
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@/app/_data/store'
 
 const Wrapper = ({ children }) => {
   return (
     <StyledComponentsRegistry>
       <ConfigProvider>
-        {children}
+        <ReduxProvider store={store}>
+          {children}
+        </ReduxProvider>
       </ConfigProvider>
     </StyledComponentsRegistry>
   );
