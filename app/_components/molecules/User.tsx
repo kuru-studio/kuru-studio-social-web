cabdafe53b8991e3aff166b97fa7252fb2cae23a
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-02-15 18:47:01 +0800

    Feature: Add Testing and Unit Tests (#49)
    # Story Title
    
    [This is the Issue Title](https://github.com/kuru-studio/kuru-studio-social-web/issues/1)
    
    ## Changes made
    
    - made this
    - did that
    
    ## How does the solution address the problem
    
    This PR will...
    
    - Add JEST as the Testing framework
    - Add some Unit Tests for existing components 
    - Note there is an issue on github-react-button, not having standard js syntax

--- app/_components/molecules/User.tsx
+++ app/_components/molecules/User.tsx
@@ -1,12 +1,20 @@
 import Atom from "@atom";
 
-const User = () => {
+const User: React.FunctionComponent = () => {
   return (
     <div className="flex gap-3 m-3 md:m-0">
-      <div className="grow md:grow-0"><Atom.Button block className="bg-white text-black">Log In</Atom.Button></div>
-      <div className="grow md:grow-0"><Atom.Button block className="bg-white text-black">Register</Atom.Button></div>
+      <div className="grow md:grow-0">
+        <Atom.Button block type="primary">
+          Log In
+        </Atom.Button>
+      </div>
+      <div className="grow md:grow-0">
+        <Atom.Button block type="primary">
+          Register
+        </Atom.Button>
+      </div>
     </div>
   );
-}
+};
 
 export default User;
--- app/_components/organisms/Footer.tsx
+++ app/_components/organisms/Footer.tsx
@@ -1,7 +1,7 @@
-import { HeartIcon } from '@heroicons/react/solid';
-import Atom from '@atom';
+import { HeartIcon } from "@heroicons/react/solid";
+import Atom from "@atom";
 
-const Footer = () => {
+const Footer: React.FunctionComponent = () => {
   return (
     <footer className="shadow-lg">
       <div className="bg-gray-600 text-white">
@@ -11,13 +11,17 @@ const Footer = () => {
       </div>
       <div className="bg-gray-800 text-white">
         <div className="container mx-auto py-3 flex flex-col items-center gap-1 text-sm">
-          <div className="flex items-center gap-1">Made with <HeartIcon className="h-5 w-5 text-red-500" /> by</div>
+          <div className="flex items-center gap-1">
+            Made with <HeartIcon className="h-5 w-5 text-red-500" /> by
+          </div>
           <div>Kuru Studio Team and the Kuru Studio Community</div>
-          <div className="mt-2"><Atom.GitHub /></div>
+          <div className="mt-2">
+            <Atom.GitHub />
+          </div>
         </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
