2594f049d699d24abdda6caf3fc97ea2fef70cdd
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-06-05 11:11:03 +0800

    add authReducer to Redux store

--- app/_data/store.ts
+++ app/_data/store.ts
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '@/app/_data/counter'
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "@/app/_data/counter";
+import authReducer from "@/app/_data/firebase-auth";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
   },
-})
+});
